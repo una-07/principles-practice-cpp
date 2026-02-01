f47ff18b49720ff71a2463dfe737760dfb5119f9
una-07 <esirkepulpan8@gmail.com>
2026-02-01 17:15:03 +0500

    Create ReportGenerator.h

---
+++ ReportGenerator.h
@@ -0,0 +1,16 @@
+#ifndef REPORT_GENERATOR_H
+#define REPORT_GENERATOR_H
+
+
+#include <iostream>
+
+
+class ReportGenerator {
+public:
+void generateReport() {
+std::cout << "Generating report..." << std::endl;
+}
+};
+
+
+#endif
